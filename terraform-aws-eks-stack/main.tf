2b1b0da0dc405e7badce050e1244e0a1411b8f8c
lalalili12 <yuvalamra12@gmail.com>
2026-01-20 14:03:30 +0200

    adding private_azs to vpc module

--- terraform-aws-eks-stack/main.tf
+++ terraform-aws-eks-stack/main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
+  private_azs     = var.private_azs
 }
 
 module "iam" {
