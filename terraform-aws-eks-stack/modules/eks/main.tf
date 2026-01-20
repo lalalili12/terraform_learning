11b93ed6b40f7508eb15a6c73e60cab298dd67c3
lalalili12 <yuvalamra12@gmail.com>
2026-01-20 15:38:16 +0200

    add ami type

--- terraform-aws-eks-stack/modules/eks/main.tf
+++ terraform-aws-eks-stack/modules/eks/main.tf
@@ -37,6 +37,7 @@ resource "aws_eks_node_group" "this" {
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.private_subnet_ids
   instance_types = var.instance_types 
+  ami_type       = "AL2_x86_64"
 
   scaling_config {
     desired_size = 2
