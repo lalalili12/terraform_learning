8ed0f1104470b85ea5ee3b30300f78b1121283f1
lalalili12 <yuvalamra12@gmail.com>
2026-01-20 12:12:03 +0200

    variables cant be used in backend

--- terraform-aws-eks-stack/backend.tf
+++ terraform-aws-eks-stack/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   # Configure the backend to use Amazon S3 for storing the Terraform state file
   backend "s3" {
-    bucket         = "${var.project_name}-terraform-state-bucket" # Name of the S3 bucket
-    key            = "${var.project_name}/terraform.tfstate" # Path to the state file within the bucket
-    region         = "${var.aws_region}"               # AWS region where the S3 bucket is located
+    bucket         = "shay-terraform-state-bucket" # Name of the S3 bucket
+    key            = "eks-stack/terraform.tfstate" # Path to the state file within the bucket
+    region         = "eu-central-1"               # AWS region where the S3 bucket is located
     dynamodb_table = "terraform-locks"            # DynamoDB table for state locking and consistency
     encrypt        = true                          # Enable server-side encryption for the state file
   }
