7974b43f30fb18f2b620166b6fd5a8d90e16ebba
lalalili12 <yuvalamra12@gmail.com>
2026-01-20 19:54:20 +0200

    adding outputs

--- terraform-aws-eks-stack/modules/eks/outputs.tf
+++ terraform-aws-eks-stack/modules/eks/outputs.tf
@@ -9,3 +9,11 @@ output "cluster_endpoint" {
 output "node_sg_id" {
   value = aws_security_group.eks_nodes.id
 }
+
+output "instance_types" {
+  value = aws_eks_node_group.this.instance_types
+}
+
+output "release_version" {
+  value = aws_eks_node_group.this.release_version 
+}
